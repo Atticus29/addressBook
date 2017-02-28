dc3a588741ef8172390daebfbb089bcc408547db
Gloria Friesen and Mark Fisher <gloria@>
2017-02-28 08:43:34 -0800

    edit the resetFields function to remove all contents of new-addresses div and then populate with one blank new-addresses div

--- js/scripts.js
+++ js/scripts.js
@@ -22,28 +22,31 @@ Address.prototype.fullAddress = function() {
 function resetFields() {
   $("input#new-first-name").val("");
   $("input#new-last-name").val("");
-  $("input.new-street").val("");
-  $("input.new-city").val("");
-  $("input.new-state").val("");
+  $("#new-addresses").empty();
+  addNewAddress();
+}
+
+function addNewAddress(){
+  $("#new-addresses").append('<div class="new-address">' +
+                               '<div class="form-group">' +
+                                 '<label for="new-street">Street</label>' +
+                                 '<input type="text" class="form-control new-street">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-city">City</label>' +
+                                 '<input type="text" class="form-control new-city">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-state">State</label>' +
+                                 '<input type="text" class="form-control new-state">' +
+                               '</div>' +
+                             '</div>');
 }
 
 //user interface logic
 $(document).ready(function() {
   $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-street">Street</label>' +
-                                   '<input type="text" class="form-control new-street">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-city">City</label>' +
-                                   '<input type="text" class="form-control new-city">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-state">State</label>' +
-                                   '<input type="text" class="form-control new-state">' +
-                                 '</div>' +
-                               '</div>');
+    addNewAddress();
   });
   $("form#new-contact").submit(function() {
     event.preventDefault();
